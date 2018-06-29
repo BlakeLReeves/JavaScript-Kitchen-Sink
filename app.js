9c99a6ff7f3ce0599f86ea8a3c6f31dbbc58a220
blakereeves19 <blake.reeves19@gmail.com>
2018-06-28 21:56:05 -0500

    Added two functions

--- app.js
+++ app.js
@@ -17,3 +17,18 @@ if(name.charCodeAt(0) >= 76) {
 } else {
     alert('Next!');
 }
+
+function sayHello() {
+    alert('Hello World!');
+}
+sayHello();
+
+function checkAge(name2, age) {
+    if(age < 21) {
+        alert(`Sorry ${name2}, you aren't old enough to view this page!`);
+    }
+}
+checkAge('Charles', 21)
+checkAge('Abby', 27)
+checkAge('James', 18)
+checkAge('John', 17)
